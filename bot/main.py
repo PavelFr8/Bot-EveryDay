93c3d7143df19471f89f769d0ba7c37386a32b3e
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-07-21 04:47:13 +0300

    fix

--- bot/main.py
+++ bot/main.py
@@ -28,7 +28,7 @@ async def main():
     # Configure logging
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(level)s - %(name)s - %(message)s"
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     )
 
     # Creating DB engine for PostgreSQL
