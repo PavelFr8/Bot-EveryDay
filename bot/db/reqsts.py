1ad164c35a2b6f31b29f0a484aaede8e3814f6c0
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-07-23 01:01:49 +0300

    fix requests to db

--- bot/db/reqsts.py
+++ bot/db/reqsts.py
@@ -35,30 +35,26 @@ async def save_data(session: AsyncSession, user_id: int, data: Dict = None):
     user = await session.execute(
         select(UserData).where(UserData.user_id == str(user_id))
     )
-    user = user.scalars().first()
+    user: UserData = user.scalars().first()
     if bool(user):
         logger.info(f'Update user {user_id}')
         if data:
             if data["deals"] != '':
-                user.deals_list = user.deals_list + "),(" + '0' + f'{data["deals"]}'
+                if user.deals_list == '':
+                    user.deals_list = '0' + data["deals"]
+                else:
+                    user.deals_list = user.deals_list + "),(" + '0' + data["deals"]
             if data['notifications'] != '':
-                user.notification_list = user.notification_list + "),(" + data['notifications']
+                if user.notification_list == '':
+                    user.notification_list = data["notifications"]
+                else:
+                    user.notification_list = user.notification_list + "),(" + data['notifications']
     else:
         logger.info(f'New user {user_id}')
         entry = UserData()
         entry.user_id = str(user_id)
-        if data:
-            if data['deals']:
-                entry.deals_list = data['deals']
-            else:
-                entry.deals_list = ''
-            if data['notifications']:
-                entry.notification_list = data['notifications']
-            else:
-                entry.notification_list = ''
-        else:
-            entry.deals_list = ''
-            entry.notification_list = ''
+        entry.deals_list = ''
+        entry.notification_list = ''
         session.add(entry)
     try:
         await session.commit()
