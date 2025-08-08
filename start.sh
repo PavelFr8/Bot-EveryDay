e5b014bb5ade1d66573eada92fac852172571d0c
Pavel <151254599+PavelFr8@users.noreply.github.com>
2025-08-08 22:11:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 echo "Проверяем доступность бота..."
 
-# Пытаемся достучаться до бота с таймаутом 5 секунд
 if wget --spider --timeout=5 --tries=3 http://bot.railway.internal:8000/; then
   echo "Бот доступен, запускаем nginx..."
   exec nginx -g 'daemon off;'
