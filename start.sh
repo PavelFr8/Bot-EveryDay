25e094e4c26103e642799b58cb07b57fd7181cc3
Pavel <151254599+PavelFr8@users.noreply.github.com>
2025-08-08 22:40:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
-
-echo "Проверяем доступность бота..."
+echo "Проверяем доступность бота по HTTP..."
 
 if wget --spider --timeout=5 --tries=3 http://bot.railway.internal:8000/; then
-  echo "Бот доступен, запускаем nginx..."
-  exec nginx -g 'daemon off;'
+  echo "Бот доступен"
 else
-  echo "Бот недоступен, перезапускаемся через 10 секунд..."
-  sleep 10
-  exec /start.sh
+  echo "Бот недоступен"
 fi
+
+echo "Проверка с curl:"
+curl -6 -I --max-time 5 http://bot.railway.internal:8000/
+
+echo "Проверка порта 8000 с помощью nc:"
+nc -z -v -w5 bot.railway.internal 8000 && echo "Порт открыт" || echo "Порт закрыт или нет доступа"
